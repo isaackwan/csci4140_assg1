a59aa315448f494b87ed4c98595fde55b52c936b
Isaac Kwan <g@isaac.pw>
2018-01-27 15:09:26 +0800

    random progress

---
+++ cgi-bin/html_helpers.py
@@ -0,0 +1,8 @@
+def header():
+	print '<!DOCTYPE HTML>' \
+		+ '<html><head>' \
+		+ '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" crossorigin="anonymous">' \
+		+ '</head><body><div class="container">\n'
+
+def footer():
+	print '\n</div></body></html>'
