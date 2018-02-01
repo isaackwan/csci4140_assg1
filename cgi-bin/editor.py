a3cee0698b2867db748d0d39d1d036792d87e83f
Isaac Kwan <g@isaac.pw>
2018-02-01 13:17:53 +0800

    progress in editor

--- cgi-bin/editor.py
+++ cgi-bin/editor.py
@@ -4,6 +4,8 @@ from pprint import pprint
 import cgi
 import cgitb
 import sqlite3
+from os import environ
+from subprocess import check_call
 from math import ceil
 from html_helpers import header, footer
 
@@ -15,17 +17,21 @@ conn = sqlite3.connect('insta.db')
 c = conn.cursor()
 
 def new_url(delta):
-	old_url = form.fp.getvalue() # type: str
+	old_url = environ['QUERY_STRING'] # type: str
 	old_url = old_url.rsplit('=', 1)
 	return old_url[0] + '=' + str(int(old_url[1]) + delta)
 
-print new_url(-1)
-
 if 'action' in form:
 	if form['action'].value == 'undo':
-		print 'undo'
+		url = '{}?{}'.format(environ['SCRIPT_NAME'], new_url(-1))
+		print '<meta http-equiv="refresh" content="0; url={}">'.format(cgi.escape(url, True))
+		print '<h1>Please wait while you\'re being redirected...</h1>'
+	elif form['action'].value == 'discard':
+		check_call("rm upload_temp/{id}_*.jpg".format(id=form['id'].value), shell=True)
+	elif form['action'].value == 'finish':
+		pass
 	else:
-		print 'unknown action'
+		print '\n unknown action'
 
 with open('editor.html', 'r') as f:
 	print f.read()
