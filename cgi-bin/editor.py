60024e0e634da6c43fb4470c3bfd790f0018c237
Isaac Kwan <g@isaac.pw>
2018-02-01 16:49:05 +0800

    not bad

--- cgi-bin/editor.py
+++ cgi-bin/editor.py
@@ -4,8 +4,10 @@ from pprint import pprint
 import cgi
 import cgitb
 import sqlite3
-from os import environ
+from os import environ, rename
 from subprocess import check_call
+from sys import exit
+from Cookie import SimpleCookie
 from math import ceil
 from html_helpers import header, footer
 
@@ -15,21 +17,42 @@ print 'Content-type: text/html\n'
 form = cgi.FieldStorage()
 conn = sqlite3.connect('insta.db')
 c = conn.cursor()
+cookie = SimpleCookie(environ['HTTP_COOKIE'])
 
 def new_url(delta):
 	old_url = environ['QUERY_STRING'] # type: str
 	old_url = old_url.rsplit('=', 1)
-	return old_url[0] + '=' + str(int(old_url[1]) + delta)
+	sequence_new = int(old_url[1]) + delta
+	return old_url[0] + '=' + str(sequence_new)
 
 if 'action' in form:
 	if form['action'].value == 'undo':
-		url = '{}?{}'.format(environ['SCRIPT_NAME'], new_url(-1))
+		if int(form['sequence'].value) <= 0:
+			print "<h1>Nothing to undo</h1>"
+			exit()
+
+		path_old = 'upload_temp/{id}_{sequence}.{ext}'.format(id=form['id'].value, sequence=int(form['sequence'].value)-1, ext=form['file_ext'].value)
+		path_new = 'upload_temp/{id}_{sequence}.{ext}'.format(id=form['id'].value, sequence=int(form['sequence'].value)+1, ext=form['file_ext'].value)
+		rename(path_old, path_new)
+		url = '{}?{}'.format(environ['SCRIPT_NAME'], new_url(1))
 		print '<meta http-equiv="refresh" content="0; url={}">'.format(cgi.escape(url, True))
-		print '<h1>Please wait while you\'re being redirected...</h1>'
+		print '<h2>Please wait while you\'re being redirected...</h2>'
 	elif form['action'].value == 'discard':
-		check_call("rm upload_temp/{id}_*.jpg".format(id=form['id'].value), shell=True)
+		check_call("rm upload_temp/{id}_*.{ext}".format(id=form['id'].value, ext=form['file_ext'].value), shell=True)
+		print '<meta http-equiv="refresh" content="0; url=/">'
+		print '<h1>Successfully discarded</h1>'
+		print '<h2>Please wait while you\'re being redirected...</h2>'
 	elif form['action'].value == 'finish':
-		pass
+		if 'username' not in cookie or len(cookie['username'].value) == 0:
+			print "<h1>You are not logged in</h1>"
+			exit()
+		new_link = 'uploads/{id}.{ext}'.format(id=form['id'].value, ext=form['file_ext'].value)
+		c.execute("INSERT INTO photos (link, username, private) VALUES (?, ?, ?)", ('/' + new_link, cookie['username'].value, form['private'].value))
+		rename('upload_temp/{id}_{sequence}.{ext}'.format(id=form['id'].value, sequence=form['sequence'].value, ext=form['file_ext'].value), new_link)
+		conn.commit()
+		print '<meta http-equiv="refresh" content="0; url=/">'
+		print '<h1>Successfully saved</h1>'
+		print '<h2>Please wait while you\'re being redirected...</h2>'
 	else:
 		print '\n unknown action'
 
--- cgi-bin/upload.py
+++ cgi-bin/upload.py
@@ -23,15 +23,16 @@ cookie = SimpleCookie(os.environ['HTTP_COOKIE'])
 if form['file'].file:
 	identify_output = check_output(["identify", "-"], stdin=form['file'].file)
 	if ('PNG' in identify_output and form['file'].filename[-4:] == '.png') or \
-	   ('JPEG' in identify_output and form['file'].filename[-4:] == '.jpg') or \
-	   ('JPEG' in identify_output and form['file'].filename[-5:] == '.jpeg'):
+		('GIF' in identify_output and form['file'].filename[-4:] == '.gif') or \
+		('JPEG' in identify_output and form['file'].filename[-4:] == '.jpg') or \
+		('JPEG' in identify_output and form['file'].filename[-5:] == '.jpeg'):
 		id = str(uuid4())
 		file_ext = form['file'].filename.split('.')[-1]
 		with open('upload_temp/{}_0.{}'.format(id, file_ext), 'wb') as fout:
 			form['file'].file.seek(0)
 			copyfileobj(form['file'].file, fout, 100000)
 		print 'Nice! Copied Over. Bringing you to the editor page...'
-		print '<meta http-equiv="refresh" content="0; url=/cgi-bin/editor.py?id={}&file_ext={}&sequence=0">'.format(quote(id), quote(file_ext))
+		print '<meta http-equiv="refresh" content="0; url=/cgi-bin/editor.py?id={}&file_ext={}&private={}&sequence=0">'.format(quote(id), quote(file_ext), quote(form['private'].value))
 
 	else:
 		print 'Rejecting because file type does not match'
