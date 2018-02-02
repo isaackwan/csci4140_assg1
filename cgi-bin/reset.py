77ee937a1aa08538c58ce70ee7602a682782f289
Isaac Kwan <g@isaac.pw>
2018-02-02 09:16:27 +0800

    reset implementation

---
+++ cgi-bin/reset.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2
+
+from pprint import pprint
+import cgi
+import cgitb
+import sqlite3
+from Cookie import SimpleCookie
+from subprocess import call
+from html_helpers import header, footer
+
+cgitb.enable()
+print 'Content-type: text/html'
+
+form = cgi.FieldStorage()
+conn = sqlite3.connect('insta.db')
+cursor = conn.cursor()
+cursor.execute("PRAGMA foreign_keys = ON")
+cookie = SimpleCookie()
+
+if 'confirm' not in form:
+	print '\n<h1>Please go to /init.html</h1>'
+	exit()
+
+cookie['username'] = ''
+print cookie
+print '\n'
+
+cursor.execute('DELETE FROM users')
+conn.commit()
+
+call('rm upload_temp/* uploads/*', shell=True)
+
+print '<meta http-equiv="refresh" content="0; url=/">'
+print '<h1>Reset success</h1>'
