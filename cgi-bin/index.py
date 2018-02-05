0183ee1f5022b069dfa8e284d4030ad3e15ea0aa
Isaac Kwan <g@isaac.pw>
2018-02-05 16:51:24 +0800

    removed typo

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -25,7 +25,7 @@ print '<link rel="stylesheet" href="/css/index.css">'
 print '<nav class="navbar navbar-dark bg-primary navbar-expand">'
 
 if not_logged_in:
-	print '<ul class="navbar-nav"><li class="nav-item"><a class="nav-link active" href="/login.html">Login</a></li><li class="nav-item"><a class="nav-link active" href="/register.htmls">Register</a></li></ul>'
+	print '<ul class="navbar-nav"><li class="nav-item"><a class="nav-link active" href="/login.html">Login</a></li><li class="nav-item"><a class="nav-link active" href="/register.html">Register</a></li></ul>'
 else:
 	print '<span class="navbar-text">Hello, {username}!</span><ul class="navbar-nav"><li class="nav-item"><a class="nav-link active " href="/cgi-bin/logout.py">Logout</a></li></ul>'.format(username=cookie['username'].value)
 print '</nav>'
