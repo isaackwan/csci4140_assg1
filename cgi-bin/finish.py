60ab48b81ebffd9a3b5363f0d83b93946fceee6f
Isaac Kwan <g@isaac.pw>
2018-02-05 18:07:52 +0800

    proper form close for finish page

--- cgi-bin/finish.py
+++ cgi-bin/finish.py
@@ -15,7 +15,7 @@ header()
 
 print '<link rel="stylesheet" href="/static/index.css">'
 print '<a role="button" class="btn btn-primary btn-lg btn-block" href="/">Back</a>'
-print '<form><div class="form-group"><input class="form-control" value="{}"></div>'.format(environ['CSCI4140_URL'] + '/' + cgi.escape(form['url'].value))
+print '<form><div class="form-group"><input class="form-control" value="{}"></div></form>'.format(environ['CSCI4140_URL'] + '/' + cgi.escape(form['url'].value))
 print '<a href="{}">'.format(cgi.escape('/' + form['url'].value))
 print '<img src="{}" class="img-thumbnail">'.format(cgi.escape('/' + form['url'].value))
 print '</a>'
